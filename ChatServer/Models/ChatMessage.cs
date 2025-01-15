f63ab5e4c5759ee22973b2211501d973435ae16d
Thiago Fortini <tiago.fortini784@gmail.com>
2025-01-15 00:47:47 -0300

    ...

---
+++ ChatServer/Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatServer.Models;
+
+public class ChatMessage
+{
+    public string From { get; init; }
+    public string To { get; init; }
+    public string Message { get; init; }
+    public bool IsMine { get; init; }
+}
